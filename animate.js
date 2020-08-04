148eaf2b52563a9482800c209354ad28375198e0
Zainul-Aziz <64695371+Zainul-Aziz@users.noreply.github.com>
2020-08-04 11:00:35 +0530

    Create animate.js
    adding animation using waypoint.js

---
+++ animate.js
@@ -0,0 +1,69 @@
+$(document).ready(function()
+  {
+    $('.about_set').waypoint(function(direction){
+      $('.about_set').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    });
+    $('.coronaDetails').waypoint(function(direction){
+      $('.coronaDetails').addClass('animated slideInRight')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.sym1').waypoint(function(direction){
+      $('.sym1').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.sym2').waypoint(function(direction){
+      $('.sym2').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.sym3').waypoint(function(direction){
+      $('.sym3').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.sym4').waypoint(function(direction){
+      $('.sym4').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.sym5').waypoint(function(direction){
+      $('.sym5').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.sym6').waypoint(function(direction){
+      $('.sym6').addClass('animated zoomIn')
+    }, {
+        offset:'60%'
+    }
+    );
+
+    $('.prevention1').waypoint(function(direction){
+      $('.prevention1').addClass('animated slideInLeft')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.prevention2').waypoint(function(direction){
+      $('.prevention2').addClass('animated fadeInUp')
+    }, {
+        offset:'60%'
+    }
+    );
+    $('.prevention3').waypoint(function(direction){
+      $('.prevention3').addClass('animated slideInRight')
+    }, {
+        offset:'60%'
+    }
+    );
+  });
